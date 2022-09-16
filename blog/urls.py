4aa2f95322555cae4c1cc500c009fd2a154c8566
zaios <andys0623@gmail.com>
2022-09-16 15:33:35 +0800

    swagger open api

--- blog/urls.py
+++ blog/urls.py
@@ -16,14 +16,34 @@ Including another URLconf
 #https://www.django-rest-framework.org/api-guide/authentication/
 
 from django.contrib import admin
+from django.conf.urls import url
 from django.urls import path, include, re_path
-from personal import views
+from rest_framework import permissions
 from rest_framework.authtoken import views
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+# from personal import views
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="blog API",
+      default_version='v1',
+      description="blog API Description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
+ 	url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 	path('api-token-auth/', views.obtain_auth_token), 
     path('admin/', admin.site.urls),
-    path('personal/', include('personal.urls', namespace='personal')),
+    # path('personal/', include('personal.urls', namespace='personal')),
     path('api/', include('blog.router')),
 ]
 # https://blog.typeart.cc/django%E8%88%87django%20REST%20framework(DRF)%E9%96%8B%E7%99%BC%E7%AD%86%E8%A8%98/
